5f1c9fbb710075905f185d2f4c91fadd286e0f84
loureq177 <lorenc.maciek.ml@gmail.com>
2024-04-02 22:08:25 +0200

    teraz stronka wygląda tak

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout>
     <div class="container mx-auto flex flex-col items-center justify-center">
-      <h1 class="text-4xl font-bold">Hello World</h1>
+      <h1 class="text-4xl font-bold">karol patrz, umiem gita</h1>
     </div>
   </NuxtLayout>
 </template>
