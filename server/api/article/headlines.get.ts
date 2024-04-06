43fe75403b1667ac300e38cb9029d0f06d0c748c
Szczepaneria <thecamoboi@gmail.com>
2024-04-06 13:54:53 +0200

    Added fetch for top headlines

---
+++ server/api/article/headlines.get.ts
@@ -0,0 +1,37 @@
+const API_KEY = '41a71c2c07364a64abf785aa5afa02ca'
+
+interface IArticleSource {
+  id: string
+  name: string
+}
+
+interface IArticle {
+  source: IArticleSource
+  author: string
+  title: string
+  description: string
+  url: string
+  urlToImage: string
+  publishedAt: string
+  content: string
+}
+
+interface IArticleResponse {
+  status: string
+  totalResults: number
+  articles: IArticle[]
+}
+
+export default defineEventHandler(async (event) => {
+  const id = getRouterParam(event, 'id')
+
+  const res = await $fetch<IArticleResponse>(
+    `https://newsapi.org/v2/top-headlines?q=${id}&searchIn=title&apiKey=${API_KEY}`
+  )
+  
+
+  return {
+    statusCode: 200,
+    results: res.articles
+  }
+})
