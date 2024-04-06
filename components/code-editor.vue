ddb4254e023b43746f5ae4c7766f885f0588250d
loureq177 <lorenc.maciek.ml@gmail.com>
2024-04-06 23:45:13 +0200

    edytor kodu

---
+++ components/code-editor.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <iframe
+      src="https://www.onlinegdb.com/"
+      frameborder="0"
+      style="border: 0; width: 100%; height: 100vh;"
+      allowfullscreen>
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OnlineGDBIframe'
+};
+</script>
