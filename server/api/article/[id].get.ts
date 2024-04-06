b53e85111ef8b2d6a002d4d993ab7c0810e1d6e8
loureq177 <lorenc.maciek.ml@gmail.com>
2024-04-06 13:41:54 +0200

    endpoints made by iwo

---
+++ server/api/article/[id].get.ts
@@ -0,0 +1,34 @@
+const API_KEY = '41a71c2c07364a64abf785aa5afa02ca'
+
+interface IArticleSource {
+  id: string
+  name: string
+}
+
+interface IArticle {
+  source: IArticleSource
+  author: string
+  title: string
+  description: string
+  url: string
+  urlToImage: string
+  publishedAt: string
+  content: string
+}
+
+interface IArticleResponse {
+  status: string
+  totalResults: number
+  articles: IArticle[]
+}
+
+export default defineEventHandler(async (event) => {
+  const id = getRouterParam(event, 'id')
+
+  const res = await $fetch<IArticleResponse>(`https://newsapi.org/v2/everything?q=${id}&searchIn=title&apiKey=${API_KEY}`)
+
+  return {
+    statusCode: 200,
+    results: res.articles
+  }
+})
