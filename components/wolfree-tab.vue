6adf5fc7287a488224b521ab2a603af75762cf4e
Szczepaneria <thecamoboi@gmail.com>
2024-04-06 23:34:14 +0200

    Create wolfree-tab.vue
    Added wolfree template

---
+++ components/wolfree-tab.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="game-tab flex justify-center items-start w-full h-full">
+    <iframe
+      id="game"
+      :src="`https://wolfreealpha.gitlab.io/input/`"
+      class="w-full h-full aspect-[9/16]"
+      frameBorder="0"
+      allowFullScreen
+    ></iframe>
+  </div>
+</template>
