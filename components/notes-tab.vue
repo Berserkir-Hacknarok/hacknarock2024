c64d859e498c407fa7693c8b40fcf22ff359b087
Octoturge <iwo.strzebonski@gmail.com>
2024-04-07 03:29:10 +0200

    Added notes update

--- components/notes-tab.vue
+++ components/notes-tab.vue
@@ -1,30 +1,57 @@
 <script setup lang="ts">
 import { FwbButton, FwbSelect } from 'flowbite-vue'
 
+import type { OptionsType } from 'flowbite-vue/components/FwbSelect/types.js'
+
 const message = ref()
-const noteId = ref()
+const noteId = ref('-1')
+
+const $i18n = useI18n()
+
 const { data: notes } = useLazyAsyncData('notes', async () =>
-  (await $fetch('/api/notes')).results.map((r) => ({ ...r, name: `(${r.value}) ${r.name.slice(0, 20)}` }))
+  (await $fetch('/api/notes')).results
+    .map((r) => ({ ...r, name: `(${r.value}) ${r.name.slice(0, 20)}` }))
+    .sort((a, b) => a.value - b.value)
+)
+
+const notesOptions = computed(
+  () =>
+    [
+      { value: -1, name: $i18n.t('new') },
+      ...(notes.value?.map((note) => ({ value: String(note.value), name: note.name })) || [])
+    ] as unknown as OptionsType[]
 )
 
 async function submit() {
-  const { results } = await $fetch('/api/notes', {
+  await $fetch('/api/notes', {
     method: 'POST',
-    body: { note: message.value }
+    body: { note: message.value, id: Number(noteId.value || -1) }
   })
-  console.info(message.value)
-  console.info(results)
+
+  await refreshNuxtData('notes')
 }
 
 function reset() {
   message.value = ''
 }
+
+watch(noteId, (id) => {
+  const note = notes.value?.find((n) => Number(n.value) === Number(id))
+
+  if (note) {
+    message.value = note.note
+  } else {
+    message.value = ''
+  }
+})
 </script>
+
 <template>
   <div class="notes-tab flex flex-col w-full h-full gap-4">
     {{ notes }}
 
-    <FwbSelect v-model="noteId" :options="notes" />
+    <FwbSelect v-model="noteId" :options="notesOptions" />
+
     <textarea
       id="notes-element"
       v-model="message"
